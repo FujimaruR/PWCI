212893748605acf3f4418063cf945dfa139146ac
Jakycpp <mlucerojaqueline20@gmail.com>
2023-08-27 15:54:43 -0600

    Login y Registro

---
+++ Front/login.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="http://localhost/prueba/PWCI/Dependencias/bootstrap-5.3.0-alpha3-dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="http://localhost/prueba/PWCI/Dependencias/bootstrap-5.3.0-alpha3-dist/css/bootstrap.min.css">
+    <title>Micherry</title>
+    <link rel="stylesheet" href="http://localhost/prueba/PWCI/Front/css/login.css">
+    <link rel="shortcut icon" href="http://localhost/prueba/PWCI/img/logo/Micherry.png">
+
+    <style>
+        body{ background-image: url(http://localhost/prueba/PWCI/img/login/fondoLogReg.gif);
+                background-repeat: no-repeat;
+                background-attachment: fixed;
+                background-size: cover;
+                background-position: center;
+        }
+    </style>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<div class="container-fluid">
+    <div class="row justify-content-center align-items-center min-vh-100">
+        <div class="col-sm-8 col-md-6 col-lg-4 transparent-bg py-5 rounded ">
+          <div class="col-12 text-center">
+             <img class="mx-auto mb-3" src="http://localhost/prueba/PWCI/img/logo/Micherry.png" alt="Centered Image" style="max-width: 150px;">
+            <h4>Micherry</h4>
+          </div>
+            <form action="" class="col-8 mx-auto">
+                  <input type="email" class="form-control my-3" id="correoLogin" placeholder="name@example.com" required>
+
+                  <input type="Password" class="form-control my-3" id="PasswordLogin"name="Password" placeholder="password" required> 
+
+                  <div class="form-check mb-3">
+                    <input type="checkbox" class="form-check-input" id="rememberMe">
+                    <label class="form-check-label" for="rememberMe">Recordar cuenta</label>
+                  </div>
+                  
+                  <div class="col-12 text-center py-3">
+                    <button type="submit" class="btn btn-danger mt-3">Sign in</button>
+                  </div> 
+
+            </form>
+            <div class="col-8  mx-auto align-items-center">
+              <label>No tienes cuenta?</label><a href="registro.php" style="color:brown">Regístrate</a>
+            </div>
+        </div>
+    </div>
+  </div>
+</body>
+</html>
---
+++ Front/registro.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="http://localhost/prueba/PWCI/Dependencias/bootstrap-5.3.0-alpha3-dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="http://localhost/prueba/PWCI/Dependencias/bootstrap-5.3.0-alpha3-dist/css/bootstrap.min.css">
+    <title>Micherry</title>
+    <link rel="stylesheet" href="http://localhost/prueba/PWCI/Front/css/login.css">
+    <link rel="shortcut icon" href="http://localhost/prueba/PWCI/img/logo/Micherry.png">
+
+    <style>
+        body{ background-image: url(http://localhost/prueba/PWCI/img/login/fondoLogReg.gif);
+                background-repeat: no-repeat;
+                background-attachment: fixed;
+                background-size: cover;
+                background-position: center;
+        }
+    </style>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<div class="container-fluid">
+    <div class="row justify-content-center align-items-center min-vh-100 py-5">
+        <div class="col-sm-8 col-md-6 col-lg-6 transparent-bg py-5 rounded ">
+          <div class="col-12 text-center">
+             <img class="mx-auto mb-3" src="http://localhost/prueba/PWCI/img/logo/Micherry.png" alt="Centered Image" style="max-width: 150px;">
+            <h4>Micherry</h4>
+          </div>
+          <form action="" class="row g-3">
+              <div class="col-12 text-center align-items-center">
+                <h5>Registro de usuario</h5>
+             </div>
+                <div class="col-1 mx-4"></div>
+                <div class="col-4">
+                    <input type="email" class="form-control " id="correoLogin" placeholder="name@example.com" required>
+                    
+                    <input type="text" class="form-control my-2" id="correoLogin" placeholder="usuario" required>
+
+                    <input type="Password" class="form-control my-2" id="PasswordLogin"name="Password" placeholder="password" required>
+                   
+                    <input type="text" class="form-control my-2" id="correoLogin" placeholder="nombre completo" required>
+  
+
+                </div>
+                <div class="col-4">
+                  <label for="formFile" class="form-label">Foto de perfil</label>
+                    <div class="card">
+                      <input class="form-control" style="background-size: 50vh" type="file" id="#img-preview" onchange="loadFile(event)" required>
+                      <img id="#img-uploader"/>
+                    </div>
+                </div>
+                <div class="col-12 text-center">
+                  <div class="form-check form-switch d-flex my-2 ">
+                    <label class="form-check-label px-5 " for="genderSwitch">Mujer</label>
+                    <input class="form-check-input px-3" type="checkbox" id="genderSwitch">
+                    <label class="form-check-label" for="genderSwitch">Hombre</label>
+                  </div>
+                  <label for="form-label" class="form-label col-sm-8 col-md-5 col-lg-5 px-5">Fecha de nacimiento
+                    <input type="date" class="form-control" id="R_FECHA" name="R_FECHA" required>
+                </div>
+                  <div class="col-12 text-center">
+                    <div class="form-check mb-3">
+                     <label class="form-check-label" for="rememberMeR">Recordar cuenta
+                     <input type="checkbox" class="form-check-input" id="rememberMeR"></label> 
+
+                    </div>
+                    <button type="submit" class="btn btn-danger mt-3">Sign in</button>
+                  </div> 
+            </form>
+            <div class="col-sm-8 mx-auto text-center">
+              <label >Ya tienes cuenta?</label><a href="login.php"style="color:brown">Inicia sesion</a>
+            </div>
+        </div>
+    </div>
+  </div>
+  <script src="js/cargaImagen.js"></script>   
+</body>
+</html>
