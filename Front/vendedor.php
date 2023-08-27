4756f14f9e0b3145c783bdec79d17b443c97b714
FujimaruR <yair.castillo.p1@gmail.com>
2023-08-26 18:11:59 -0600

    vendedor

---
+++ Front/vendedor.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="http://localhost/prueba/PWCI/Dependencias/bootstrap-5.3.0-alpha3-dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="http://localhost/prueba/PWCI/Dependencias/bootstrap-5.3.0-alpha3-dist/css/bootstrap.min.css">
+    <title>Vendedor</title>
+</head>
+<body>
+    
+</body>
+</html>
