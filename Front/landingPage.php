0b4c40ac96a9a0f7d50f906554320596f9c616ac
Jakycpp <mlucerojaqueline20@gmail.com>
2023-08-28 22:49:50 -0600

    Landing Page

---
+++ Front/landingPage.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="http://localhost/prueba/PWCI/Dependencias/bootstrap-5.3.0-alpha3-dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="http://localhost/prueba/PWCI/Dependencias/bootstrap-5.3.0-alpha3-dist/css/bootstrap.min.css">
+    <title>Micherry</title>
+    <link rel="stylesheet" href="http://localhost/prueba/PWCI/Front/css/landing.css">
+    <link rel="shortcut icon" href="http://localhost/prueba/PWCI/img/logo/Micherry.png">
+<style>
+     body {
+        overflow-x: hidden; /* Evita la barra de desplazamiento horizontal */
+        background: linear-gradient(to left, #F2AFA0, #F2CEA2);
+     }
+</style>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+<div class="container-fluid px-0">
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+
+        <button
+            class="navbar-toggler"
+            type="button"
+            data-mdb-toggle="collapse"
+            data-mdb-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+        >
+            <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse mx-4" id="navbarSupportedContent">
+
+            <a class="navbar-brand mt-2 mt-lg-0 text-white" href="#">
+            <img
+                src="http://localhost/prueba/PWCI/img/logo/Micherry.png"
+                height="50"
+                alt="MDB Logo"
+                loading="lazy"
+            />  Micherry
+            </a>
+        </div>
+
+        <div class="d-flex align-items-center">
+            <a href="registro.php" class="btn btnColorCard btnHover text-white ">Registrate</a>
+        </div>
+        <div class="d-flex align-items-center">
+            <a href="login.php" class="btn btnColorCard btnHover text-white mx-5">Iniciar sesión</a>
+        </div>
+    </nav>
+
+
+</div>
+
+    <div class="row" style="position: relative; background-color: black;">
+        <div class="background-image" style="background-image: url('http://localhost/prueba/PWCI/img/landingPageImg/Landingimg.gif'); opacity: 0.7;">
+
+        </div>            
+            <div class="centered-text">
+                <h1 class="letraFuente">Bienvenido a Micherry</h1>
+                <h2 class="letraFuente">¡Tenemos el mejor catálogo de productos lindos del mundo!</h2>
+                <a href="registro.php" class="btn btnColorCard btnHover text-white my-3">Ver productos</a>
+               
+            </div>
+    </div>
+        <div class="row ">
+            <div class="text-center letraFuente "style="color:#5d0c48">
+                <h2 class="py-5">¡Disfruta de todos los articulos que tenemos para ti!</h2>
+                <h4> Manejamos ropa de calidad </h4>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="image-container text-center">
+                <img src="http://localhost/prueba/PWCI/img/landingPageImg/sueterRosa.jpg" alt="Imagen 1">
+                <img src="http://localhost/prueba/PWCI/img/landingPageImg/sueter2.jpg" alt="Imagen 2">
+                <img src="http://localhost/prueba/PWCI/img/landingPageImg/sueter3.jpg" alt="Imagen 3">
+            </div>
+        </div>
+
+        <div class="row "style="background: linear-gradient(to left, #F2AFA0, #F2CEA2);color:#5d0c48">
+            <div class="text-center letraFuente ">
+                <h2 class="py-5">¡Productos escolares!</h2>
+                <h4>Mochilas con accesorios</h4>
+            </div>
+        </div>
+        <div class="row"style="background: linear-gradient(to left, #F2AFA0,#F2CEA2); ">
+            <div class="image-container text-center " >
+                <img src="http://localhost/prueba/PWCI/img/landingPageImg/mochila.jpg" alt="Imagen 1">
+                <img src="http://localhost/prueba/PWCI/img/landingPageImg/mochila2.jpg" alt="Imagen 2">
+                <img src="http://localhost/prueba/PWCI/img/landingPageImg/mochila3.jpg" alt="Imagen 3">
+            </div>
+        </div>
+        <div class="row ">
+            <div class="text-center letraFuente " style="color:#5d0c48">
+                <h2 class="py-5">¡Área de Informática!</h2>
+                <h4>Computadoras, teclados, consolas...</h4>
+            </div>
+        </div>
+        <div class="row">
+            <div class="image-container text-center">
+                <img src="http://localhost/prueba/PWCI/img/landingPageImg/consola.jpg" alt="Imagen 1">
+                <img src="http://localhost/prueba/PWCI/img/landingPageImg/teclado.jpg" alt="Imagen 2">
+                <img src="http://localhost/prueba/PWCI/img/landingPageImg/Mac.jpg" alt="Imagen 3">
+            </div>
+        </div>
+        <div class="text-center letraFuente "style="background: linear-gradient(to left, #F2AFA0,#F2CEA2); color:#5d0c48">
+            <h2 class="py-5">¡Y mucho más!</h2>
+            <h4>Inicia sesión para ver nuestra pagina principal</h4>
+            <a href="login.php" class="btn btnColorCard btnHover text-white mx-5 my-5">Iniciar sesión</a>
+        </div>
+    </div>
+ 
+    <footer class="bg-dark text-white p-5">
+    <div class="container Footer">
+        <div class="row">
+            <div class="col-md-3 col-lg-3 text-reset text-uppercase d-flex align-items-center">
+                <!-- Logo inferior -->
+                <img src="http://localhost/prueba/PWCI/img/logo/Micherry.png" width="60" alt="Logo Micherry" class="img-logo mr-2">
+                Micherry
+            </div>
+
+            <div class="col-md-3 col-lg-3">
+                <!-- Menú 1 -->
+                <ul class="list-unstyled">
+                    <li class="my-2">1975922</li>
+                    <li class="my-2">Lucero Jaqueline Mendoza Alejandro</li>
+                </ul>
+            </div>
+
+            <div class="col-md-3 col-lg-3">
+                <!-- Menú 2 -->
+                <ul class="list-unstyled">
+                    <li class="my-2"></li>
+                    <li class="my-2">1975922</li>
+                    <li class="my-2">Emilio Yair Castillo Pacheco</li>
+                </ul>
+            </div>
+
+            <div class="col-md-3 col-lg-3">
+                <!-- Menú 3 -->
+                <ul class="list-unstyled">
+                    <li class="my-3">Materia</li>
+                    <li class="my-2">Programación web de capa intermedia</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+
+</body>
+</html>
