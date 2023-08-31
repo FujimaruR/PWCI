7338f1493bd097416360e4ffb57f7090ff9e453f
FujimaruR <yair.castillo.p1@gmail.com>
2023-08-31 00:21:20 -0600

    cambios

---
+++ Front/administrador.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="http://localhost/prueba/PWCI/Front/css/administrador.css">
+  <script
+    src="http://localhost/prueba/PWCI/Dependencias/bootstrap-5.3.0-alpha3-dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet"
+    href="http://localhost/prueba/PWCI/Dependencias/bootstrap-5.3.0-alpha3-dist/css/bootstrap.min.css">
+  <title>Micherry</title>
+  <link rel="shortcut icon" href="http://localhost/prueba/PWCI/img/logo/Micherry.png">
+</head>
+<body>
+    
+</body>
+</html>
