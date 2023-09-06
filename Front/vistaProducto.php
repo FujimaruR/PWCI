4598cc35f353700a62f187385d2fdfe9026972d6
FujimaruR <yair.castillo.p1@gmail.com>
2023-09-06 11:43:17 -0600

    arreglo

--- Front/vistaProducto.php
+++ Front/vistaProducto.php
@@ -10,9 +10,6 @@
   <link rel="stylesheet"
     href="http://localhost/prueba/PWCI/Dependencias/bootstrap-5.3.0-alpha3-dist/css/bootstrap.min.css">
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
@@ -200,7 +197,7 @@
                               <ul class="dropdown-menu">
                                 <li><a class="dropdown-item" href="#">Lista 1</a></li>
                                 <li><a class="dropdown-item" href="#">Lista 2</a></li>
-                                <li><a class="dropdown-item" href="#" data-toggle="modal" data-target="#crearLista">Crear lista</a></li>
+                                <li><a class="dropdown-item" data-bs-toggle="modal" data-bs-target="#crearLista">Crear lista</a></li>
                               </ul>
                             </div>
                         </div>
@@ -252,7 +249,7 @@
                             </div>
                         </div>
                         <!-- Crear Lista  -->
-                        <div class="modal fade" id="crearLista">
+                        <div class="modal fade" id="crearLista" tabindex="-1" aria-labelledby="crearLista" aria-hidden="true">
                             <div class="modal-dialog">
                             <div class="modal-content">
                             
